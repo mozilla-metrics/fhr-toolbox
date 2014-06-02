ee54e5fa16f096facb837d597e88b1a6db7dedf1
Benjamin Smedberg <benjamin@smedbergs.us>
2014-06-02 12:47:49 -0400

    Move jydoop->mrjob

--- mrjob/searches_by_engine.py
+++ mrjob/searches_by_engine.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# THIS SCRIPT IS OUT OF DATE: IT NEES TO BE PORTED TO USE MRJob instead of
+# jydoop
+
 import jydoop
 
 from healthreportutils import (
