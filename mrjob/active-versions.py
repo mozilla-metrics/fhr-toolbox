774573404d0fdcd38312686a1cb9d564cea8aacd
Benjamin Smedberg <benjamin@smedbergs.us>
2015-03-17 15:37:24 +0000

    Add script to count active user over a 30-day window and their version.

---
+++ mrjob/active-versions.py
@@ -0,0 +1,165 @@
+import healthreportutils
+from datetime import date, datetime, timedelta
+import os, shutil, csv
+import sys, codecs
+import traceback
+
+import mrjob
+from mrjob.job import MRJob
+import tempfile
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+main_channels = (
+    'nightly',
+    'aurora',
+    'beta',
+    'release'
+)
+
+def last_saturday(d):
+    """Return the Saturday on or before the date."""
+    # .weekday in python starts on 0=Monday
+    return d - timedelta(days=(d.weekday() + 2) % 7)
+
+def start_date(dstr):
+    """
+    Start measuring a few days before the snapshot was taken to give clients
+    time to upload.
+    """
+    snapshot = datetime.strptime(dstr, "%Y-%m-%d").date()
+    startdate = last_saturday(snapshot) - timedelta(days=28)
+    return startdate
+
+def date_back(start, days):
+    """iter backwards from start for N days"""
+    date = start
+    for n in xrange(0, days):
+        yield date - timedelta(days=n)
+
+def active_day(day):
+    if day is None:
+        return False
+    return any(k != "org.mozilla.crashes.crashes" for k in day)
+
+@healthreportutils.FHRMapper()
+def map(job, key, payload):
+    channel = payload.channel.split("-")[0]
+    if channel not in main_channels:
+        return
+
+    days = payload.get('data', {}).get('days', {})
+    def get_day(d):
+        dstr = d.strftime("%Y-%m-%d")
+        return days.get(dstr, None)
+
+    version = payload.get("geckoAppInfo", {}).get("version", "?")
+    sd = start_date(job.options.start_date)
+
+    active = False
+    last_update = None
+
+    for d in date_back(sd, 30):
+        day = get_day(d)
+        if active_day(day):
+            active = True
+            if not last_update and "org.mozilla.appInfo.update" in day:
+                last_update = day
+
+    if not last_update:
+        last_update = {}
+
+    update_auto = last_update.get("org.mozilla.appInfo.update", {}).get("autoDownload", "?")
+    update_enabled = last_update.get("org.mozilla.appInfo.update", {}).get("enabled", "?")
+
+    osname = payload.last.get("org.mozilla.sysinfo.sysinfo", {}).get("name", "?")
+
+    yield (("active", osname, version, update_auto, update_enabled), 1)
+
+def reduce(job, k, vlist):
+    yield (k, sum(vlist))
+
+class AggJob(MRJob):
+    HADOOP_INPUT_FORMAT="org.apache.hadoop.mapred.SequenceFileAsTextInputFormat"
+    INPUT_PROTOCOL = mrjob.protocol.RawProtocol
+
+    def run_job(self):
+        self.stdout = tempfile.TemporaryFile()
+
+        if self.options.start_date is None:
+            raise Exception("--start-date is required")
+        # validate the start date here
+        start_date(self.options.start_date)
+
+        # Do the big work
+        super(AggJob, self).run_job()
+
+        # Produce the separated output files
+        outpath = self.options.output_path
+        if outpath is None:
+            outpath = os.path.expanduser("~/fhr-activeversions-" + self.options.start_date)
+        output(self.stdout, outpath)
+
+    def configure_options(self):
+        super(AggJob, self).configure_options()
+
+        self.add_passthrough_option('--output-path', help="Specify output path",
+                                    default=None)
+        self.add_passthrough_option('--start-date', help="Specify start date",
+                                    default=None)
+
+    def mapper(self, key, value):
+        return map(self, key, value)
+
+    def reducer(self, key, vlist):
+        return reduce(self, key, vlist)
+
+    combiner = reducer
+
+def getresults(fd):
+    fd.seek(0)
+    for line in fd:
+        k, v = line.split("\t")
+        yield json.loads(k), json.loads(v)
+
+def unwrap(l, v):
+    """
+    Unwrap a value into a list. Dicts are added in their repr form.
+    """
+    if isinstance(v, (tuple, list)):
+        for e in v:
+            unwrap(l, e)
+    elif isinstance(v, dict):
+        l.append(repr(v))
+    elif isinstance(v, unicode):
+        l.append(v.encode("utf-8"))
+    else:
+        l.append(v)
+
+def output(fd, path):
+    try:
+        shutil.rmtree(path)
+    except OSError:
+        pass
+    os.mkdir(path)
+
+    writers = {}
+    errs = codecs.getwriter("utf-8")(open(os.path.join(path, "exceptions.txt"), "w"))
+    for k, v in getresults(fd):
+        l = []
+        unwrap(l, k)
+        unwrap(l, v)
+        fname = l.pop(0)
+        if fname in writers:
+            w = writers[fname]
+        else:
+            fd = open(os.path.join(path, fname + ".csv"), "w")
+            w = csv.writer(fd)
+            writers[fname] = w
+        w.writerow(l)
+
+if __name__ == '__main__':
+    AggJob.run()
